f83e4d87130e82747bba14e2e2542b82de785961
Din Lanh <langdinhieu@gmail.com>
2019-06-28 23:03:36 -0400

    added login functionality

--- private/auth_functions.php
+++ private/auth_functions.php
@@ -6,6 +6,8 @@
     session_regenerate_id();
     $_SESSION['admin_id'] = $admin['id'];
     $_SESSION['last_login'] = time();
+    $_SESSION['username'] = $admin['username'];
+
     return true;
   }
 
